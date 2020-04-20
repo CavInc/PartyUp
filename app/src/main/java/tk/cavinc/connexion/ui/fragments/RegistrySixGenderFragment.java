cea75c139fcf0eac71ac16baaebce054eaba880a
Alexander <cavinc20@gmail.com>
2020-04-20 15:26:01 +0700

    Работа над дизайном

--- app/src/main/java/tk/cavinc/connexion/ui/fragments/RegistrySixGenderFragment.java
+++ app/src/main/java/tk/cavinc/connexion/ui/fragments/RegistrySixGenderFragment.java
@@ -7,6 +7,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import tk.cavinc.connexion.R;
 
@@ -15,16 +16,31 @@ import tk.cavinc.connexion.R;
  */
 
 public class RegistrySixGenderFragment extends BaseFragment implements View.OnClickListener {
+    private Button mSixMale;
+    private Button mSixFemale;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.registry_six_fragment, container, false);
-
+        mSixMale = rootView.findViewById(R.id.six_male_bt);
+        mSixMale.setOnClickListener(this);
+        mSixFemale = rootView.findViewById(R.id.six_female_bt);
+        mSixFemale.setOnClickListener(this);
+        rootView.findViewById(R.id.registry_next).setOnClickListener(this);
         return rootView;
     }
 
     @Override
     public void onClick(View view) {
+        if (view.getId() == R.id.registry_next) {
 
+        }
+        if (view.getId() == R.id.six_female_bt) {
+            mSixFemale.setBackground(getResources().getDrawable(R.drawable.button_red_bg));
+        }
+        if (view.getId() == R.id.six_male_bt) {
+            mSixMale.setBackground(getResources().getDrawable(R.drawable.button_red_bg));
+        }
     }
 }
