04f267842888843f82f9a67e6a16c39dc42a618a
Alexander <cavinc20@gmail.com>
2020-04-28 15:37:55 +0700

    Добавлена проверка наличия сети Добавлена установка разрешения на запись в хранилище

--- app/src/main/java/tk/cavinc/connexion/ui/activitys/MainActivity.java
+++ app/src/main/java/tk/cavinc/connexion/ui/activitys/MainActivity.java
@@ -1,7 +1,12 @@
 package tk.cavinc.connexion.ui.activitys;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +14,8 @@ import tk.cavinc.connexion.R;
 import tk.cavinc.connexion.ui.fragments.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +31,24 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction trz = getSupportFragmentManager().beginTransaction();
         trz.replace(R.id.container,fragment,tag);
         trz.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkAndSetPermission();
+    }
+
+    private void checkAndSetPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
